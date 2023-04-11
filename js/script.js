ef4fe69c56b72ca9d1c1a0a9dc11375cf8523438
Viktoryia Vysotskaya <radevich.vika2014@gmail.com>
2023-04-11 15:43:04 +0200

    Add title-list in script.js

--- js/script.js
+++ js/script.js
@@ -50,6 +50,69 @@ const titleClickHandler = function (event) {
 
 const links = document.querySelectorAll('.titles a');
 
+console.log(links);
+
 for (let link of links) {
   link.addEventListener('click', titleClickHandler);
 }
+
+
+const optArticleSelector = '.post',
+  optTitleSelector = '.post-title',
+  optTitleListSelector = '.titles';
+
+function generateTitleLinks() {
+
+  /* remove contents of titleList */
+
+  const titleList = document.querySelector(optTitleListSelector);
+  titleList.innerHTML = '';
+
+
+  /* for each article */
+
+  let html = '';
+
+  const articles = document.querySelectorAll(optArticleSelector);
+
+  for (let article of articles) {
+
+
+    /* get the article id */
+
+    const article = document.querySelector(optArticleSelector);
+    const articleId = article.getAttribute('id');
+
+    console.log(articleId);
+
+    /* find the title element */
+
+    /* get the title from the title element */
+
+    const articleTitle = article.querySelector(optTitleSelector).innerHTML;
+
+
+    /* create HTML of the link */
+
+    const linkHTML = '<li><a href="#' + articleId + '"><span>' + articleTitle + '</span></a></li>';
+
+    console.log(linkHTML);
+
+    /* insert link into titleList */
+
+    html = html + linkHTML;
+
+  }
+
+  titleList.innerHTML = html;
+
+  console.log(html);
+
+  const links = document.querySelectorAll('.titles a');
+
+  for (let activeLink of activeLinks) {
+    activeLink.classList.remove('active');
+  }
+}
+
+generateTitleLinks();
